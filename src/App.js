68c3c1382e7402a5ec34689f6d588a778f95d151
richardebrain <richardebrain20@gmail.com>
2022-03-26 16:40:09 +0100

    updated our app with header component and added logo that leads to home and shop page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Homepage from './pages/homepage/hompPage.component';
 import { Route ,Routes,Link} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './component/header/header.component';
 
 function App() {
   return (
     <div>
+     <Header/>
       <Routes>
         <Route path='/' element={<Homepage/>}/>
         <Route path='shop' element={<ShopPage/>}/>
